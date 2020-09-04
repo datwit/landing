b3083fcd8d0a5f45018369acddfa9f332d07011f
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2020-09-03 21:24:37 -0400

    style: :art: Visual components redesign
    About us page: Team members section redesign. Home page: Redoing the team section: Using the fullwidth illustration with the memebers floating over it. Reducing members's picture a little. Footer section: Apply ing proper alignment to the email & icon. Services page & Case studies page: Reviewing the line height of the paragraphs and the padding of the sections. Floating call to action button from the banner area: Applying pulse MD effect and tooltip. Trying hoverable attribute for cards. Applied it on the blog post cards to see how it looks. Added  a Go to up floating button on the homepage.
    
    #27

--- themes/agico-hugo/static/plugins/wittylytics/script.js
+++ themes/agico-hugo/static/plugins/wittylytics/script.js
@@ -22,6 +22,31 @@ $(document).ready(function() {
   $('input#name,input#email,input#subject,textarea#textarea1').characterCounter();
  }); 
 
+// Got t top button 
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+// tooltip floating call to action button banner
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var options = [exitDelay= 1];
+  var instances = M.Tooltip.init(elems, options);
+});
+
 /* function changedValue() {
   let text = document.getElementById("textarea1");
   let textValue = text.value;
