12edf1e0129d2c92acf095061588923caf9550f3
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2020-09-25 16:26:58 -0400

    style: :art: Redesigning services page
    Stating layout. Adding components: Services section and keyword bullet section.
    
    #72

--- themes/agico-hugo/static/plugins/datwit/script.js
+++ themes/agico-hugo/static/plugins/datwit/script.js
@@ -61,6 +61,22 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Materialbox.init(elems, options);
 });
 
+//collapsible
+var accItem = document.getElementsByClassName('accordionItem');
+    var accHD = document.getElementsByClassName('accordionItemHeading');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'accordionItem close';
+        }
+        if (itemClass == 'accordionItem close') {
+            this.parentNode.className = 'accordionItem open';
+        }
+    }
+
 
 /* function changedValue() {
   let text = document.getElementById("textarea1");
