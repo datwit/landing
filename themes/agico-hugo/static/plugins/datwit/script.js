83fed4e0e482564c8dfb6fa39d8bb6d03a41f26d
Enma Lidia Munoz Garcia <enmalidia88@gmail.com>
2020-10-05 15:17:54 -0400

    style: :art: Services page: diagram section
    Adding styles for selected dots. Updating js function. Aligning dots.
    
    #72

--- themes/agico-hugo/static/plugins/datwit/script.js
+++ themes/agico-hugo/static/plugins/datwit/script.js
@@ -83,78 +83,171 @@ var accHD = document.getElementsByClassName('accordionItemHeading');
   var r = document.getElementById("demo2");
   var s = document.getElementById("demo3");
   var m = document.getElementById("demo4");
+
+  var tit1 = document.getElementById("tit1");
+  var tit2 = document.getElementById("tit2");
+  var tit3 = document.getElementById("tit3");
+  var tit4 = document.getElementById("tit4");
+
+  var dot1 = document.getElementById("dot1");
+  var dot2 = document.getElementById("dot2");
+  var dot3 = document.getElementById("dot3");
+  var dot4 = document.getElementById("dot4");
+
+
   function toggleText(res) {    
     switch(res){
       /***************onclick button 1**************************** */
       case 1:
         if ((t.className == 'text hide') & (r.className == 'text hide') & (s.className=='text hide') & (m.className=='text hide')) {
-          t.className = 'text show';      
+          t.className = 'text show';  
+          dot1.className = 'dot1 draw-orange'; 
+          tit1.className = 'tit1 bold-title';
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2';
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3'; 
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';           
         } 
         else{
           t.className = 'text hide';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1';
         }
         if ((t.className == 'text show') | (r.className == 'text show') | (s.className=='text show') | (m.className=='text show')){
           t.className = 'text show';
+          dot1.className = 'dot1 draw-orange';
+          tit1.className = 'tit1 bold-title'; 
           r.className = 'text hide';
           s.className = 'text hide';
-          m.className = 'text hide';      
+          m.className = 'text hide'; 
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2';
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3'; 
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';       
         }
         else{
           t.className = 'text hide';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1';
         }   
         break;
         /***********************onclick button 2****************** */
       case 2:
         if ((r.className == 'text hide') & (t.className == 'text hide') & (s.className=='text hide') & (m.className=='text hide')) {
-          r.className = 'text show';      
+          r.className = 'text show';  
+          dot2.className = 'dot2 draw-orange'; 
+          tit2.className = 'tit2 bold-title'; 
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1';
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3'; 
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';           
         } 
         else{
           r.className = 'text hide';
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2'; 
+
         }
         if ((r.className == 'text show') | (t.className == 'text show') | (s.className=='text show') | (m.className=='text show')){
           r.className = 'text show';
+          dot2.className = 'dot2 draw-orange'; 
+          tit2.className = 'tit2 bold-title';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1'; 
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3'; 
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';       
           t.className = 'text hide';
           s.className = 'text hide';
           m.className = 'text hide';      
         }
         else{
           r.className = 'text hide';
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2'; 
         }
         break;
       /*********************onclick button 3*********************** */  
       case 3:
         if ((s.className == 'text hide') & (t.className == 'text hide') & (r.className=='text hide') & (m.className=='text hide')) {
-          s.className = 'text show';      
+          s.className = 'text show';  
+          dot3.className = 'dot3 draw-orange'; 
+          tit3.className = 'tit3 bold-title'; 
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1'; 
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';     
         } 
         else{
           s.className = 'text hide';
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3'; 
         }
         if ((s.className == 'text show') | (t.className == 'text show') | (r.className=='text show') | (m.className=='text show')){
           s.className = 'text show';
           t.className = 'text hide';
           r.className = 'text hide';
-          m.className = 'text hide';      
+          m.className = 'text hide';  
+          dot3.className = 'dot3 draw-orange'; 
+          tit3.className = 'tit3 bold-title'; 
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1'; 
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';      
         }
         else{
           s.className = 'text hide';
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3'; 
         }
         break;
        /*********************onclick button 4************************* */ 
       case 4:
         if ((m.className == 'text hide') & (t.className == 'text hide') & (r.className=='text hide') & (s.className=='text hide')) {
-          m.className = 'text show';      
+          m.className = 'text show'; 
+          dot4.className = 'dot4 draw-orange'; 
+          tit4.className = 'tit4 bold-title';  
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1'; 
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3';     
         } 
         else{
           m.className = 'text hide';
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';    
         }
         if ((m.className == 'text show') | (t.className == 'text show') | (r.className=='text show') | (s.className=='text show')){
           m.className = 'text show';
           t.className = 'text hide';
           r.className = 'text hide';
-          s.className = 'text hide';      
+          s.className = 'text hide'; 
+          dot4.className = 'dot4 draw-orange'; 
+          tit4.className = 'tit4 bold-title';  
+          dot2.className = 'dot2'; 
+          tit2.className = 'tit2';
+          dot1.className = 'dot1'; 
+          tit1.className = 'tit1'; 
+          dot3.className = 'dot3'; 
+          tit3.className = 'tit3';        
         }
         else{
           m.className = 'text hide';
+          dot4.className = 'dot4'; 
+          tit4.className = 'tit4';   
         }
         break;
       default:
